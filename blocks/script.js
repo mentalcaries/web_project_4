68a412d5163f54d8e36b2c549365f60d86d3438f
Devin Jaggernauth <neexis@gmail.com>
2021-06-13 13:29:13 -0400

    truncate image titles

--- blocks/script.js
+++ blocks/script.js
@@ -1,17 +1,13 @@
 let popupForm = document.querySelector(".popup");
 let edit = document.querySelector(".profile__edit-btn");
 let close = document.querySelector(".popup__close-btn");
-console.log(edit)
-console.log(popupForm)
 
 
 
 function showPopup() {
-  popupForm.classList.toggle('popup_visible');
+  popupForm.classList.toggle('popup_opened');
 }
 
 edit.addEventListener("click", showPopup);
 close.addEventListener("click", showPopup);
-
-
 
