c08fe278e88332dadf59f596a9d165616f7fc15e
Devin Jaggernauth <neexis@gmail.com>
2021-06-12 23:30:05 -0400

    modal styling

--- blocks/script.js
+++ blocks/script.js
@@ -1,6 +1,6 @@
 let popupForm = document.querySelector(".popup");
 let edit = document.querySelector(".profile__edit-btn");
-let close = document.querySelector(".close-btn");
+let close = document.querySelector(".popup__close-btn");
 console.log(edit)
 console.log(popupForm)
 
