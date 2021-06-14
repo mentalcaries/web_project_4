7db918e4985dbe62857d59c8b0980577b7d2c4f0
Devin Jaggernauth <neexis@gmail.com>
2021-06-14 11:22:31 -0400

    complete form

--- blocks/script.js
+++ blocks/script.js
@@ -1,13 +1,49 @@
+//Button variables
 let popupForm = document.querySelector(".popup");
-let edit = document.querySelector(".profile__edit-btn");
-let close = document.querySelector(".popup__close-btn");
+let editBtn = document.querySelector(".profile__edit-btn");
+let closeBtn = document.querySelector(".popup__close-btn");
+let saveBtn = document.querySelector(".popup__save-btn");
+let overlay = document.querySelector(".popup__overlay");
 
+//Page Text
+let profileName = document.querySelector(".profile__name");
+let profileTitle = document.querySelector(".profile__subtitle");
 
+//Form Text
+let popupName = document.querySelector("#popup_name");
+let popupTitle = document.querySelector("#popup_title");
 
+//Show and hide popup
 function showPopup() {
-  popupForm.classList.toggle('popup_opened');
+  popupForm.classList.add('popup_opened');
+  popupName.value = profileName.textContent;
+  popupTitle.value = profileTitle.textContent;
 }
 
-edit.addEventListener("click", showPopup);
-close.addEventListener("click", showPopup);
+//Hide popup 
+function hidePopup() {
+  popupForm.classList.remove('popup_opened');
+}
 
+//Save data to HTML, Close popup
+function saveInfo(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileTitle.textContent = popupTitle.value;
+  hidePopup();
+}
+
+editBtn.addEventListener("click", showPopup);
+closeBtn.addEventListener("click", hidePopup);
+overlay.addEventListener("click", hidePopup);
+saveBtn.addEventListener("click", saveInfo);
+
+// On form opening:
+// Form text fields should have values from HTML input
+// profile__name === popupName 
+// profile__title === popupTitle
+
+//On form saving:
+//    Text from form fields should be added back to HTML
+  // popupName = profile__name
+  // popUptitle = profile__title
