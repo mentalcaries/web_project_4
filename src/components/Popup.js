000d1789cfbeef8b11c822ebf3fb3f9d5e749efd
Devin Jaggernauth <neexis@gmail.com>
2021-08-24 21:12:13 -0400

    escClose callback function, remove unused constants

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,17 +1,18 @@
 class Popup {
   constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popupElement.classList.add("popup_opened");
-    document.addEventListener("keyup", this._handleEscClose.bind(this));
+    document.addEventListener("keyup", this._handleEscClose);
 
   }
 
   close() {
     this._popupElement.classList.remove("popup_opened");
-    document.removeEventListener("keyup", this._handleEscClose.bind(this));
+    document.removeEventListener("keyup", this._handleEscClose);
   }
 
   _handleEscClose(evt) {
